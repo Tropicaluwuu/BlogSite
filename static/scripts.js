0cfdefec08e271b9f0b20d145c5fb65d43fabfe1
Reggie Rodriguez <rodpalg@dunwoody.edu>
2025-12-10 16:14:39 -0600

    Added more crazy stuff

--- static/scripts.js
+++ static/scripts.js
@@ -1,10 +1,35 @@
-function likePost(postId) {
-  const select = document.getElementById("likeUserSelect");
-  if (!select || !select.value) {
-    alert("Please select a user to like as.");
-    return;
-  }
-  const userId = select.value;
-  // Redirect to the like route
-  window.location.href = `/posts/${postId}/like/${userId}`;
+console.log("JS Loaded");
+
+
+function randomRainbowColor() {
+    const hue = Math.floor(Math.random() * 360);
+    return `hsl(${hue}, 90%, 65%)`;
 }
+
+function createSparkle(x, y) {
+    const s = document.createElement("div");
+    s.className = "sparkle";
+
+    
+    s.style.left = x + "px";
+    s.style.top = y + "px";
+
+    
+    s.style.background = randomRainbowColor();
+
+  
+    const size = 12 + Math.random() * 18;
+    s.style.width = size + "px";
+    s.style.height = size + "px";
+
+    document.body.appendChild(s);
+
+    setTimeout(() => s.remove(), 900);
+}
+
+
+document.addEventListener("pointermove", (event) => {
+    const x = event.pageX; 
+    const y = event.pageY;
+    createSparkle(x, y);
+});
